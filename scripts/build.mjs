e2fe81815605265944a712c5c4ab26f0edbe1f27
lijie <62596617+korylee@users.noreply.github.com>
2022-10-21 15:20:27 +0800

    chore: build vtools

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -5,9 +5,11 @@ import ora from 'ora'
 const CWD = process.cwd()
 const PKG_CLI = resolve(CWD, './packages/smy-h5-cli')
 const PKG_UI = resolve(CWD, './packages/smy-h5-ui')
+const PKG_VTOOLS = resolve(CWD, './packages/smy-h5-vtools')
 
 export const buildCli = () => execa('pnpm', ['build'], { cwd: PKG_CLI })
 export const buildUi = (noUmd) => execa('pnpm', ['compile', noUmd ? '--noUmd' : ''], { cwd: PKG_UI })
+export const buildVtools = () => execa('pnpm', ['build'], { cwd: PKG_VTOOLS })
 
 export async function runTask(taskName, task) {
   const s = ora().start(`Building ${taskName}`)
@@ -22,5 +24,6 @@ export async function runTask(taskName, task) {
 
 ;(async () => {
   await runTask('cli', buildCli)
+  await runTask('vtools', buildVtools)
   await runTask('ui', () => buildUi(false))
 })()
