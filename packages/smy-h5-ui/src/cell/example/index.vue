ac16579b297d7a29f83b1b37cdc1270db5328cac
lijie <62596617+korylee@users.noreply.github.com>
2022-12-22 14:12:22 +0800

    feat(cell-group): add cell group component

--- packages/smy-h5-ui/src/cell/example/index.vue
+++ packages/smy-h5-ui/src/cell/example/index.vue
@@ -1,37 +1,77 @@
 <template>
-  <div>
+  <div class="cell-example">
     <div>基础使用</div>
-    <Cell>这里是cell组件</Cell>
+    <smy-cell>这里是cell组件</smy-cell>
     <div>展示描述</div>
-    <Cell
+    <smy-cell
       >这里是cell组件
       <template #desc>这里是描述</template>
-    </Cell>
+    </smy-cell>
     <div>展示图标</div>
-    <Cell
+    <smy-cell border
       >这里是cell组件
       <template #icon>
-        <Icon><CloseSvg /></Icon>
+        <smy-icon><CloseSvg /></smy-icon>
       </template>
-    </Cell>
-    <Cell
+    </smy-cell>
+    <smy-cell
       >这里是cell组件
       <template #extra>
-        <Icon><CloseSvg /></Icon>
+        <smy-icon><CloseSvg /></smy-icon>
       </template>
-    </Cell>
+    </smy-cell>
+    <div>卡片样式</div>
+    <smy-cell insert class="insert-example"
+      >这里是cell组件
+      <template #extra>
+        <smy-icon><CloseSvg /></smy-icon>
+      </template>
+    </smy-cell>
+    <div>分组用法</div>
+    <smy-cell-group insert class="insert-example">
+      <smy-cell border clickable
+        >这里是cell组件
+        <template #desc>这里是描述</template>
+        <template #extra>
+          <smy-icon><CloseSvg /></smy-icon>
+        </template>
+      </smy-cell>
+      <smy-cell border clickable
+        >这里是cell组件
+        <template #desc>这里是描述</template>
+        <template #extra>
+          <smy-icon><CloseSvg /></smy-icon>
+        </template>
+      </smy-cell>
+      <smy-cell border clickable
+        >这里是cell组件
+        <template #desc>这里是描述</template>
+        <template #extra>
+          <smy-icon><CloseSvg /></smy-icon>
+        </template>
+      </smy-cell>
+    </smy-cell-group>
   </div>
 </template>
 
 <script>
 import Cell from '../'
 import Icon from '../../icon'
+import CellGroup from '../../cell-group'
 import CloseSvg from '../../_icon/CloseSvg.vue'
 
 export default {
   name: 'CellExample',
-  components: { Cell, Icon, CloseSvg },
+  components: { [Cell.name]: Cell, [CellGroup.name]: CellGroup, [Icon.name]: Icon, CloseSvg },
 }
 </script>
 
-<style></style>
+<style lang="less" scoped>
+.cell-example {
+  background-color: #f7f8fa;
+  height: 100vh;
+}
+.insert-example {
+  margin: 10px 10px;
+}
+</style>
