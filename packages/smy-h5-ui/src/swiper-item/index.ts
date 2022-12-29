10c8904c5a5864f991c03c0acfb9d114b2b0a632
lijie <62596617+korylee@users.noreply.github.com>
2022-12-29 22:56:35 +0800

    types: swiper item类型文件

--- packages/smy-h5-ui/src/swiper-item/index.ts
+++ packages/smy-h5-ui/src/swiper-item/index.ts
@@ -1,10 +1,6 @@
 import { withInstall } from '../_utils/components'
 import _SwiperItem from './SwiperItem.vue'
 
-import type { SmyCompoent } from '../_utils/components'
-
-declare class SwiperItem extends SmyCompoent {}
-
-const SmySwiperItem = withInstall(_SwiperItem) as unknown as SwiperItem
+const SmySwiperItem = withInstall(_SwiperItem)
 
 export default SmySwiperItem
