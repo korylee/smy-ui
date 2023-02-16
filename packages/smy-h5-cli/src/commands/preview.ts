386830b2c66fa92aefce7c03b279a860dbe6a993
lijie <62596617+korylee@users.noreply.github.com>
2023-02-16 11:40:30 +0800

    feat(cli/preview): 脚手架预览

--- packages/smy-h5-cli/src/commands/preview.ts
+++ packages/smy-h5-cli/src/commands/preview.ts
@@ -1,7 +1,7 @@
 import { pathExistsSync } from 'fs-extra'
 import { SITE_OUTPUT_PATH } from '../shared/constant'
 import logger from '../shared/logger'
-import { command } from 'execa'
+import { start } from 'live-server'
 
 export async function preview() {
   if (!pathExistsSync(SITE_OUTPUT_PATH)) {
@@ -9,7 +9,8 @@ export async function preview() {
     return
   }
   try {
-    await command('live-server --port=5500', { cwd: SITE_OUTPUT_PATH }).stdout?.pipe(process.stdout)
+    start({ port: 5500, root: SITE_OUTPUT_PATH })
+    // await command('live-server --port=5500', { cwd: SITE_OUTPUT_PATH }).stdout?.pipe(process.stdout)
   } catch (e: any) {
     logger.error(e.toString())
   }
