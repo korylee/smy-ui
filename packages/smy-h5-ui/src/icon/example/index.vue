eaab80d19f367e957364c104a3913f432a018651
lijie <62596617+korylee@users.noreply.github.com>
2023-03-23 20:04:54 +0800

    feat(ui/icon): 添加图标，补充文档

--- packages/smy-h5-ui/src/icon/example/index.vue
+++ packages/smy-h5-ui/src/icon/example/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="icon-example">
     图标尺寸
     <div>
       <smy-icon size="15" name="window-close" />
@@ -21,14 +21,28 @@
       <smy-icon color="#2979ff" size="30" :transition="300" :name="iconName" @click="toggle" />
     </div>
     组件引入使用
-    <div>
-      <smy-icon v-for="icon of iconList" :key="icon.name" size="35">
-        <component :is="icon.value" />
-      </smy-icon>
+    <div class="icon-example__icons">
+      <div
+        class="icon-example__icon smy-elevation--2"
+        v-for="icon of iconList"
+        :key="icon.name"
+        @click="handleCopySvgIcon(icon.name)"
+      >
+        <smy-icon size="35">
+          <component :is="icon.value" />
+        </smy-icon>
+      </div>
     </div>
     iconfont使用
-    <div>
-      <smy-icon v-for="icon of iconList" :key="icon.name" :name="icon.name" size="35" />
+    <div class="icon-example__icons">
+      <div
+        class="icon-example__icon smy-elevation--2"
+        v-for="icon of iconList"
+        :key="icon.name"
+        @click="handleCopyIconFont(icon.name)"
+      >
+        <smy-icon :name="icon.name" size="35" />
+      </div>
     </div>
   </div>
 </template>
@@ -38,7 +52,43 @@ import SmyIcon from '../'
 import * as icons from '@smy-h5/icons'
 import { kebabCase } from 'lodash-es'
 
-import '@smy-h5/icons/css'
+export function handleCopy(text = '') {
+  const isSupported = !!document.queryCommandSupported && !!document.queryCommandSupported('copy')
+  if (!isSupported) return false
+  const inputId = 'copy-input-el'
+  try {
+    /**
+     * @type {HTMLTextAreaElement}
+     */
+    let copyInputEl = document.getElementById(inputId)
+    if (!copyInputEl) {
+      copyInputEl = document.createElement('textarea')
+      copyInputEl.id = inputId
+      const isRTL = document.documentElement.getAttribute('dir') === 'rtl'
+      // Prevent zooming on iOS
+      copyInputEl.style.fontSize = '12pt'
+      // Reset box model
+      copyInputEl.style.border = '0'
+      copyInputEl.style.padding = '0'
+      copyInputEl.style.margin = '0'
+      // Move element out of screen horizontally
+      copyInputEl.style.position = 'absolute'
+      copyInputEl.style[isRTL ? 'right' : 'left'] = '-9999px'
+      // Move element to the same position vertically
+      const yPosition = window.pageYOffset || document.documentElement.scrollTop
+      copyInputEl.style.top = `${yPosition}px`
+      copyInputEl.setAttribute('readonly', '')
+      document.body.appendChild(copyInputEl)
+    }
+    copyInputEl.value = text
+    // copyInputEl.focus();
+    copyInputEl.setSelectionRange(0, text.length)
+    copyInputEl.select()
+    return document.execCommand('copy')
+  } catch (e) {
+    return false
+  }
+}
 
 const iconList = Object.keys(icons).map((iconName) => {
   return {
@@ -58,8 +108,44 @@ export default {
     toggle() {
       this.iconName = this.iconName === 'plus' ? 'minus' : 'plus'
     },
+    handleCopySvgIcon(name) {
+      const text = `<smy-icon><${name} /></smy-icon>`
+      const res = handleCopy(text)
+      alert(res ? `${text}复制成功！\n请按需引入该图标【${name}】` : `复制失败`)
+    },
+    handleCopyIconFont(name) {
+      const text = `<smy-icon name="${name}" />`
+      const res = handleCopy(text)
+      alert(res ? `${text} 复制成功！` : `复制失败`)
+    },
   },
 }
 </script>
 
-<style lang="less"></style>
+<style lang="less" scoped>
+@import '@smy-h5/icons/css';
+
+.icon-example {
+  &__icons {
+    width: 100%;
+    display: flex;
+    flex-wrap: wrap;
+  }
+  &__icon {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 29%;
+    padding: 6% 5%;
+    margin: 0 2% 4%;
+    cursor: pointer;
+    -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+    user-select: none;
+    border-bottom: 2px solid var(--site-color-primary);
+    transition: background-color 0.25s;
+    box-sizing: border-box;
+    box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
+    border-radius: 5px;
+  }
+}
+</style>
